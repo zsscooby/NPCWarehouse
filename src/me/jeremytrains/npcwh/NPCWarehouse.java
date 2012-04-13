a7a6bbfbe310baf97b7db712ecac7a657a0cfdb2
jeremytrains <jeremyschonfeld@gmail.com>
2012-04-13 16:11:38 -0400

    Updated src

--- src/me/jeremytrains/npcwh/NPCWarehouse.java
+++ src/me/jeremytrains/npcwh/NPCWarehouse.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.Plugin;
@@ -69,7 +70,7 @@ public class NPCWarehouse extends JavaPlugin {
 			log.log(Level.SEVERE, INTRO + "|-|-|Error Code #001|-|-|");
 			e.printStackTrace();
 			log.log(Level.SEVERE, INTRO + "==============================");
-		}
+		}	
 		log.info(INTRO + "Npc manager setup successfully!");
 		log.info(INTRO + "Loading npc data...");
 		loadAllData();
